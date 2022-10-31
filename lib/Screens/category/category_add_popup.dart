6ce763b334352cb77ed4d1d4966ff4b5224ad274
Jishad <jishadjishu200260@gmail.com>
2022-10-31 10:32:53 +0530

    Radio Button added in category pop-up screen

--- lib/Screens/category/category_add_popup.dart
+++ lib/Screens/category/category_add_popup.dart
@@ -1,18 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:sample/Models/Category/category_model.dart';
 
 Future<void> showCategoryAddPopup(BuildContext context) async {
   showDialog(
-      context: context,
-      builder: (ctx) {
-        return SimpleDialog(
-          title: const Text("Add Category"),
-          children: [
-            TextFormField(),
-            ElevatedButton(
+    context: context,
+    builder: (ctx) {
+      return SimpleDialog(
+        title: const Text("Add Category"),
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(
+                  gapPadding: 10,
+                ),
+                label: Text("Category"),
+              ),
+            ),
+          ),
+          Row(
+            children: const [
+              RadioButton(
+                  title: "Income",
+                  type: CategoryType.income,
+                  selectedCategoryType: CategoryType.income),
+              RadioButton(
+                  title: "Expence",
+                  type: CategoryType.expense,
+                  selectedCategoryType: CategoryType.income),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50.0),
+            child: ElevatedButton(
               onPressed: () {},
               child: Text("Add"),
             ),
-          ],
-        );
-      });
+          ),
+        ],
+      );
+    },
+  );
+}
+
+class RadioButton extends StatelessWidget {
+  final String title;
+  final CategoryType type;
+  final CategoryType selectedCategoryType;
+
+  const RadioButton({
+    super.key,
+    required this.title,
+    required this.type,
+    required this.selectedCategoryType,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Radio<CategoryType>(
+          value: type,
+          groupValue: CategoryType.income,
+          onChanged: (value) {
+            value = type;
+          },
+        ),
+        Text(title)
+      ],
+    );
+  }
 }
