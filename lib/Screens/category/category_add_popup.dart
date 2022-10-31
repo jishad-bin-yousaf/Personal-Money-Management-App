b30a0eff79a74c9bea541c64eea1cdf584b78435
Jishad <jishadjishu200260@gmail.com>
2022-10-31 11:42:35 +0530

    Category Pop-up UI finished

--- lib/Screens/category/category_add_popup.dart
+++ lib/Screens/category/category_add_popup.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:sample/Models/Category/category_model.dart';
 
+ValueNotifier<CategoryType> selectedCategoryNotifier = ValueNotifier(CategoryType.income);
+
 Future<void> showCategoryAddPopup(BuildContext context) async {
   showDialog(
     context: context,
@@ -19,17 +21,21 @@ Future<void> showCategoryAddPopup(BuildContext context) async {
               ),
             ),
           ),
-          Row(
-            children: const [
-              RadioButton(
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                RadioButton(
                   title: "Income",
                   type: CategoryType.income,
-                  selectedCategoryType: CategoryType.income),
-              RadioButton(
+                ),
+                RadioButton(
                   title: "Expence",
                   type: CategoryType.expense,
-                  selectedCategoryType: CategoryType.income),
-            ],
+                ),
+              ],
+            ),
           ),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 50.0),
@@ -47,24 +53,31 @@ Future<void> showCategoryAddPopup(BuildContext context) async {
 class RadioButton extends StatelessWidget {
   final String title;
   final CategoryType type;
-  final CategoryType selectedCategoryType;
 
   const RadioButton({
     super.key,
     required this.title,
     required this.type,
-    required this.selectedCategoryType,
   });
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Radio<CategoryType>(
-          value: type,
-          groupValue: CategoryType.income,
-          onChanged: (value) {
-            value = type;
+        ValueListenableBuilder(
+          valueListenable: selectedCategoryNotifier,
+          builder: (BuildContext ctx, CategoryType newCategory, Widget? _) {
+            return Radio<CategoryType>(
+              value: type,
+              groupValue: newCategory,
+              onChanged: (value) {
+                if (value == null) {
+                  return;
+                }
+                selectedCategoryNotifier.value = value;
+                selectedCategoryNotifier.notifyListeners();
+              },
+            );
           },
         ),
         Text(title)
